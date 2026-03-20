ce5c9d35e0ea41a68c9d64050b486949da89d345
Jez <146207614+jez1010@users.noreply.github.com>
2026-03-20 16:21:58 +0000

    Edited main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,42 +1,41 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express from 'express';
 
-let cachedApp: any;
+const server = express();
 
-async function bootstrap() {
-  if (!cachedApp) {
-    const app = await NestFactory.create(AppModule);
-    
-    app.enableCors({
-      origin: true,
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-      credentials: true,
-    });
+export const createNestServer = async (expressInstance) => {
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressInstance),
+  );
+  
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
 
-  if (process.env.NODE_ENV !== 'production') {
-    const port = 3000;
-    await app.listen(port, '0.0.0.0');
-    console.log(`Codespaces/Local server running on port ${port}`);
-  }
-
-    await app.init();
-    cachedApp = app.getHttpAdapter().getInstance();
-  }
-  return cachedApp;
-}
+  return app.init();
+};
 
 if (process.env.NODE_ENV !== 'production') {
   const startLocal = async () => {
     const app = await NestFactory.create(AppModule);
     app.enableCors({ origin: true, credentials: true });
-    const port = 3000;
-    await app.listen(port, '0.0.0.0');
-    console.log(`Application is running on: http://0.0.0.0:${port}`);
+    await app.listen(3000, '0.0.0.0');
+    console.log(`Application is running on: http://0.0.0.0:3000`);
   };
   startLocal();
 }
 
+let isInitialized = false;
+
 export default async (req: any, res: any) => {
-  const instance = await bootstrap();
-  instance(req, res);
+  if (!isInitialized) {
+    await createNestServer(server);
+    isInitialized = true;
+  }
+  server(req, res);
 };
